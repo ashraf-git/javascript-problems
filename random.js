4a989251f06a2ad521a86052232c4245acd55b89
ashraf-git <aliashraf.hena@gmail.com>
2020-02-27 13:09:36 +0600

    Random number, random number between 1 to 6

---
+++ random.js
@@ -0,0 +1,21 @@
+var num = 2.545;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+var dice = Math.random() * 10;
+var randomNum = Math.random() * 6;
+
+var output = Math.round(randomNum);
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(dice);
+console.log(randomNum);
+console.log(output);
+
+for( i = 0; i < 10; i++){
+    var randomNum = Math.random() * 6;
+
+var output = Math.round(randomNum);
+console.log(output);
+}
