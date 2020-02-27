3f3ed340945aca4a6667cb2910251dfdcf770614
ashraf-git <aliashraf.hena@gmail.com>
2020-02-27 16:34:13 +0600

    FIND MAX OF TWO VALUES, FIND MAX OF THREE VALUES

---
+++ max.js
@@ -0,0 +1,30 @@
+var bausiness = 650;
+
+var minister = 565;
+
+var sochib = 765
+
+var max = Math.max(bausiness, minister, sochib);
+console.log(max);
+// if(bausiness > minister){
+//     if(bausiness > sochib){
+//         console.log("business is biiger");
+
+//     }
+//     else
+//     {
+//         console.log("sochib is biigwt")
+//     }
+// }
+// else
+// {
+//     if(minister > sochib){
+//         console.log("misnister is biiger");
+//     }
+//     else{
+//         console.log("sochib is bigger")
+//     }
+
+
+
+// }
